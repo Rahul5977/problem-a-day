e042a9ac752b81169bf6355a27d522d546fb4f6a
Rahul Raj <rahul.raj9237@gmail.com>
2026-01-05 18:16:21 +0530

    Appartment

---
+++ lamda.py
@@ -0,0 +1,9 @@
+s1="Teesha"
+# lamda -> used to define anonymous functions
+# syntax -> lambda arguments: expression
+s2=lambda x: x+" Ramchandani"
+s3=s2(s1)
+print(s3)
+
+# a=lambda x: "Rahul" if x == "Yes" else "no"
+# print(a("Yes"))
