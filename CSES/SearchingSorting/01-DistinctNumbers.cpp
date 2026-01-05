892d72e4e0e0a04467aac8d7266954c90dd7c96f
Rahul Raj <rahul.raj9237@gmail.com>
2026-01-05 16:39:32 +0530

    Distinct numbers

--- CSES/SearchingSorting/01-DistinctNumbers.cpp
+++ CSES/SearchingSorting/01-DistinctNumbers.cpp
@@ -1,6 +1,18 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"Hello, World!"<<endl;
-    return 0;
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.end());
+    int ans=1;
+    for(int i=1;i<n;i++){
+        if(arr[i]!=arr[i-1]){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
 }
